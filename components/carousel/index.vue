b751cc10b5ce42371f5605baee947bad27f2c66f
tOnzm <tonselaman@gmail.com>
2024-01-24 16:45:54 +0700

    edit carousel

--- components/carousel/index.vue
+++ components/carousel/index.vue
@@ -2,13 +2,13 @@
   <div>
     <v-carousel
       cycle
-      height="400"
+      height="600"
       hide-delimiter-background
       show-arrows-on-hover
     >
       <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="img[i]" height="100%">
-          <img :src="img[i]" alt="" srcset="" width="100%" />
+        <v-sheet height="100%">
+          <img :src="img[i]" alt="" srcset="" width="100%" height="100%" />
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-h2">{{ slide }} Slide</div>
           </v-row>
@@ -25,10 +25,10 @@ export default {
     return {
       img: [
         'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
-        'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
+        'https://th.bing.com/th/id/OIP.7FybJ76ZSSkVoBWAy1WnoQHaE8?w=1080&h=720&rs=1&pid=ImgDetMain',
+        'https://th.bing.com/th/id/OIP.Jybue-ds467_33legXfcowHaD5?rs=1&pid=ImgDetMain',
+        'https://th.bing.com/th/id/OIP.Jybue-ds467_33legXfcowHaD5?rs=1&pid=ImgDetMain',
+        'https://th.bing.com/th/id/OIP.Jybue-ds467_33legXfcowHaD5?rs=1&pid=ImgDetMain',
       ],
       slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
     }
