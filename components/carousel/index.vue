061193d6940c5192d12b7dae8594dfe174d44079
tOnzm <tonselaman@gmail.com>
2024-01-24 16:40:25 +0700

    edit carousel

--- components/carousel/index.vue
+++ components/carousel/index.vue
@@ -1,59 +1,39 @@
 <template>
-    <div>
-        <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
+  <div>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
     >
-      <v-sheet
-        :color="img[i]"
-        height="100%"
-      >
-      <img :src=img[i] alt="" srcset="">
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
-    </div>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="img[i]" height="100%">
+          <img :src="img[i]" alt="" srcset="" width="100%" />
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:'CarouselComponent',
-        data () {
-      return {
-        img: [
-          'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
-          'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
+export default {
+  name: 'CarouselComponent',
+  data() {
+    return {
+      img: [
+        'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
+        'https://www.chula.ac.th/wp-content/uploads/2023/02/Banner-CUDSON-Web-App_TH-1-768x402.jpg',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
     }
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
